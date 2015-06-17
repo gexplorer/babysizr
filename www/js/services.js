7f728d94a188b73a9814d73e51ddd31ab9d45a8e
gexplorer <gexplorer8@gmail.com>
2015-06-17 19:10:46 +0200

    Added some mockup data with fakerjs

---
+++ www/js/services.js
@@ -0,0 +1,23 @@
+angular.module('services', [])
+
+    .factory('WeeksService', function () {
+        var weeks = [];
+
+        for (var n = 0; n < 10; n++) {
+            weeks.push({
+                number: n,
+                size: faker.address.zipCode() + " mm",
+                weight: faker.address.zipCode() + " gr",
+                title: faker.company.catchPhrase(),
+                avatar: faker.image.avatar(),
+                image: faker.image.image(),
+                quote: faker.lorem.paragraph()
+            });
+        }
+
+        return {
+            getWeeks: function () {
+                return weeks;
+            }
+        }
+    });
