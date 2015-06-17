f2bf3f2bdec8b2b9015311d3e302d96b88e297f2
gexplorer <gexplorer8@gmail.com>
2015-06-17 12:15:14 +0200

    No footer yet, let's focus on the content

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,5 +4,4 @@ angular.module('controllers', [])
         $scope.today = new Date();
         $scope.dueDate = new Date(2015, 11, 2);
         $scope.weekNumber = 16;
-        $scope.days = 112;
     });
