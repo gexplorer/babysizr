a41f30582ffd43f152bbda94e163c3e0f03719f1
gexplorer <gexplorer8@gmail.com>
2015-06-17 18:32:28 +0200

    Mock week in controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,4 +4,13 @@ angular.module('controllers', [])
         $scope.today = new Date();
         $scope.dueDate = new Date(2015, 11, 2);
         $scope.weekNumber = 16;
+
+        $scope.week = {
+            number: 16,
+            size: "150 mm",
+            weight: "110 gr",
+            title: "A Rubik's cube",
+            image: "rubik.png",
+            quote: "A good puzzle, it's a fair thing. Nobody it's lying. It's very clear, and the problem depends just on you."
+        }
     });
