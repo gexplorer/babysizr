2d0ac87e1b4896827b71407905e6da064753a2bd
gexplorer <gexplorer8@gmail.com>
2015-06-17 18:47:47 +0200

    Card list

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,12 +5,22 @@ angular.module('controllers', [])
         $scope.dueDate = new Date(2015, 11, 2);
         $scope.weekNumber = 16;
 
-        $scope.week = {
-            number: 16,
-            size: "150 mm",
-            weight: "110 gr",
-            title: "A Rubik's cube",
-            image: "rubik.png",
-            quote: "A good puzzle, it's a fair thing. Nobody it's lying. It's very clear, and the problem depends just on you."
-        }
+        $scope.weeks = [
+            {
+                number: 17,
+                size: "200 mm",
+                weight: "250 gr",
+                title: "Another thing",
+                image: "rubik.png",
+                quote: "Lorem ipsum."
+            },
+            {
+                number: 16,
+                size: "150 mm",
+                weight: "110 gr",
+                title: "A Rubik's cube",
+                image: "rubik.png",
+                quote: "A good puzzle, it's a fair thing. Nobody it's lying. It's very clear, and the problem depends just on you."
+            }
+        ]
     });
