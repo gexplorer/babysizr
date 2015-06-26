c42e30280f062697c0804fd16292b6b919a0fc75
gexplorer <gexplorer8@gmail.com>
2015-06-26 18:06:31 +0200

    Cleaning several warnings on swipecards

--- www/js/ionic.swipecards.js
+++ www/js/ionic.swipecards.js
@@ -16,9 +16,9 @@
     var transitionKeys = ['webkitTransition', 'transition', '-webkit-transition', 'webkit-transition',
         '-moz-transition', 'moz-transition', 'MozTransition', 'mozTransition'];
     var TRANSITION = 'webkitTransition';
-    for (var i = 0; i < transitionKeys.length; i++) {
-        if (d.style[transitionKeys[i]] !== undefined) {
-            TRANSITION = transitionKeys[i];
+    for (var j = 0; j < transitionKeys.length; j++) {
+        if (d.style[transitionKeys[j]] !== undefined) {
+            TRANSITION = transitionKeys[j];
             break;
         }
     }
@@ -26,18 +26,12 @@
     var SwipeableCardController = ionic.views.View.inherit({
         initialize: function (opts) {
             this.cards = [];
-
-            var ratio = window.innerWidth / window.innerHeight;
-
-            this.maxWidth = window.innerWidth - (opts.cardGutterWidth || 0);
             this.maxHeight = opts.height || 300;
             this.cardGutterWidth = opts.cardGutterWidth || 10;
             this.cardPopInDuration = opts.cardPopInDuration || 400;
             this.cardAnimation = opts.cardAnimation || 'pop-in';
         },
-        /**
-         * Push a new card onto the stack.
-         */
+
         pushCard: function (card) {
             var self = this;
 
@@ -49,26 +43,15 @@
                 card.disableTransition(self.cardAnimation);
             }, this.cardPopInDuration + 100);
         },
-        /**
-         * Set up a new card before it shows.
-         */
+
         beforeCardShow: function () {
             var nextCard = this.cards[this.cards.length - 1];
             if (!nextCard) return;
 
-            // Calculate the top left of a default card, as a translated pos
-            var topLeft = window.innerHeight / 2 - this.maxHeight / 2;
-            console.log(window.innerHeight, this.maxHeight);
-
-            var cardOffset = Math.min(this.cards.length, 3) * 5;
-
-            // Move each card 5 pixels down to give a nice stacking effect (max of 3 stacked)
             nextCard.setPopInDuration(this.cardPopInDuration);
             nextCard.setZIndex(this.cards.length);
         },
-        /**
-         * Pop a card from the stack
-         */
+
         popCard: function (animate) {
             var card = this.cards.pop();
             if (animate) {
@@ -79,9 +62,7 @@
     });
 
     var SwipeableCardView = ionic.views.View.inherit({
-        /**
-         * Initialize a card with the given options.
-         */
+
         initialize: function (opts) {
             opts = ionic.extend({}, opts);
 
@@ -94,55 +75,34 @@
             this.bindEvents();
         },
 
-        /**
-         * Set the X position of the card.
-         */
         setX: function (x) {
             this.el.style[ionic.CSS.TRANSFORM] = 'translate3d(' + x + 'px,' + this.y + 'px, 0)';
             this.x = x;
             this.startX = x;
         },
 
-        /**
-         * Set the Y position of the card.
-         */
         setY: function (y) {
             this.el.style[ionic.CSS.TRANSFORM] = 'translate3d(' + this.x + 'px,' + y + 'px, 0)';
             this.y = y;
             this.startY = y;
         },
 
-        /**
-         * Set the Z-Index of the card
-         */
         setZIndex: function (index) {
             this.el.style.zIndex = index;
         },
 
-        /**
-         * Set the width of the card
-         */
         setWidth: function (width) {
             this.el.style.width = width + 'px';
         },
 
-        /**
-         * Set the height of the card
-         */
         setHeight: function (height) {
             this.el.style.height = height + 'px';
         },
 
-        /**
-         * Set the duration to run the pop-in animation
-         */
         setPopInDuration: function (duration) {
             this.cardPopInDuration = duration;
         },
 
-        /**
-         * Transition in the card with the given animation class
-         */
         transitionIn: function (animationClass) {
             var self = this;
 
@@ -154,50 +114,33 @@
             }, 100);
         },
 
-        /**
-         * Disable transitions on the card (for when dragging)
-         */
         disableTransition: function (animationClass) {
             this.el.classList.remove(animationClass);
         },
 
-        /**
-         * Swipe a card out programtically
-         */
         swipe: function () {
             this.transitionOut();
         },
 
-        /**
-         * Fly the card out or animate back into resting position.
-         */
         transitionOut: function () {
             var self = this;
-
+            var direction = 1.5;
             if (this.y < 0) {
-                this.el.style[TRANSITION] = '-webkit-transform 0.2s ease-in-out';
-                this.el.style[ionic.CSS.TRANSFORM] = 'translate3d(' + this.x + ',' + (this.startY) + 'px, 0)';
-                setTimeout(function () {
-                    self.el.style[TRANSITION] = 'none';
-                }, 200);
-            } else {
-                // Fly out
-                var rotateTo = (this.rotationAngle + (this.rotationDirection * 0.6)) || (Math.random() * 0.4);
-                var duration = this.rotationAngle ? 0.2 : 0.5;
-                this.el.style[TRANSITION] = '-webkit-transform ' + duration + 's ease-in-out';
-                this.el.style[ionic.CSS.TRANSFORM] = 'translate3d(' + this.x + ',' + (window.innerHeight * 1.5) + 'px, 0) rotate(' + rotateTo + 'rad)';
-                this.onSwipe && this.onSwipe();
-
-                // Trigger destroy after card has swiped out
-                setTimeout(function () {
-                    self.onDestroy && self.onDestroy();
-                }, duration * 1000);
+                direction = -1;
             }
+            var rotateTo = (this.rotationAngle + (this.rotationDirection * -0.6)) || (Math.random() * 0.4);
+            var duration = this.rotationAngle ? 0.2 : 0.5;
+
+            this.el.style[TRANSITION] = '-webkit-transform ' + duration + 's ease-in-out';
+            this.el.style[ionic.CSS.TRANSFORM] = 'translate3d(' + this.x + ',' + (window.innerHeight * direction) + 'px, 0) rotate(' + rotateTo + 'rad)';
+            this.onSwipe && this.onSwipe();
+
+            setTimeout(function () {
+                self.onDestroy && self.onDestroy();
+            }, duration * 1000);
+
         },
 
-        /**
-         * Bind drag events on the card.
-         */
         bindEvents: function () {
             var self = this;
             ionic.onGesture('dragstart', function (e) {
@@ -225,7 +168,6 @@
             }, this.el);
         },
 
-        // Rotate anchored to the left of the screen
         _transformOriginLeft: function () {
             this.el.style[TRANSFORM_ORIGIN] = 'left center';
             this.rotationDirection = 1;
@@ -239,7 +181,7 @@
         _doDragStart: function (e) {
             var width = this.el.offsetWidth;
             var point = window.innerWidth / 2 + this.rotationDirection * (width / 2)
-            var distance = Math.abs(point - e.gesture.touches[0].pageX);// - window.innerWidth/2);
+            var distance = Math.abs(point - e.gesture.touches[0].pageX);
             console.log(distance);
 
             this.touchDistance = distance * 10;
@@ -265,7 +207,6 @@
         }
     });
 
-
     angular.module('ionic.contrib.ui.cards', ['ionic'])
 
         .directive('swipeCard', ['$timeout', function ($timeout) {
@@ -281,7 +222,6 @@
                 link: function ($scope, $element, $attr, swipeCards) {
                     var el = $element[0];
 
-                    // Instantiate our card view
                     var swipeableCard = new SwipeableCardView({
                         el: el,
                         onSwipe: function () {
@@ -293,12 +233,11 @@
                             $timeout(function () {
                                 $scope.onDestroy();
                             });
-                        },
+                        }
                     });
                     $scope.$parent.swipeCard = swipeableCard;
 
                     swipeCards.swipeController.pushCard(swipeableCard);
-
                 }
             }
         }])
@@ -309,7 +248,7 @@
                 template: '<div class="swipe-cards" ng-transclude></div>',
                 transclude: true,
                 scope: true,
-                controller: function ($scope, $element) {
+                controller: function () {
                     var swipeController = new SwipeableCardController({});
 
                     $rootScope.$on('swipeCard.pop', function (isAnimated) {
@@ -317,8 +256,6 @@
                     });
 
                     this.swipeController = swipeController;
-
-                    //return swipeController;
                 }
             }
         }])
